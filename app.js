e67a96e2bde0963e049f5c23b0b925faa3e6c7a2
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2022-07-28 15:58:32 +0300

    Javascript Inline & Internal & External

---
+++ app.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("button");
+
+btn.addEventListener("click",function(){
+    let txt = document.getElementById("text");
+    txt.textContent = "Frontend Dersleri";
+})
